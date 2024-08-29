26a00a7c0c6201b4c2cea8b1a86170a7d63fba5c
Bi7al <bilal.muhammad4573@gmail.com>
2024-08-29 23:28:50 +0500

    Minor Tone Change

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       </section>
       <section className="p-4 md:p-8 bg-white my-4 gap-4 flex flex-col md:flex-row items-center">
         <p className="font-extrabold text-2xl md:text-5xl text-[#074173] shadow-2xl p-4 rounded-2xl leading-10 h-full md:w-[45vw] mb-8">Built on Trust of our patients.<br />
-          Over 1500+ successful procedures, Serving Humanity With Our Best.
+          Over 1500+ successful procedures, Serving Our Best to Humanity.
         </p>
 
         {/* Chart Component */}
