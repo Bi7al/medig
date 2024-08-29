e46a28234aab7ec3a92a0d017430b0d9f33edc37
Bi7al <bilal.muhammad4573@gmail.com>
2024-08-29 10:38:04 +0500

    Started medig

---
+++ Components/Navbar.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Navbar() {
+    return (
+        <nav className='w-full h-16 px-4 md:px-8 bg-white shadow-lg md:shadow-2xl  fixed flex justify-between items-center'>
+            <Link href='/'><h1 className='font-extrabold text-3xl md:text-5xl text-[#3fa2f6]'>medig+</h1></Link >
+
+
+            <div className='absolute h-fit py-11 border-t-2  w-full top-16 left-0 bg-white md:static md:p-0 md:border-0 md:h-fit md:w-auto '>
+                <ul className='flex flex-col justify-center items-center ps-4 gap-3 md:flex-row md:gap-8 text-[#2066a2]'>
+                    <Link href='/panel'>
+                        <li className='uppercase font-semibold hover:underline hover:transition  ease-in delay-100 hover:underline-offset-8   decoration-[#FFC94A] hover:text-[#3FA2F6]'>doctors</li>
+                    </Link>
+                    <Link href='/schedule'>
+                        <li className='uppercase font-semibold hover:underline hover:transition  ease-in delay-100 hover:underline-offset-8   decoration-[#FFC94A] hover:text-[#3FA2F6]'>book appointment</li>
+                    </Link>
+                    <Link href='/pharmacy'>
+                        <li className='uppercase font-semibold hover:underline hover:transition  ease-in delay-100 hover:underline-offset-8   decoration-[#FFC94A] hover:text-[#3FA2F6]'>meds</li>
+                    </Link>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/Components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+
+      <body className={inter.className}>
+        <header className="h-16  w-full ">
+          <Navbar />
+        </header>
+        <main className="min-h-screen container">{children}</main>
+        <footer className="h-60  border-t-2"></footer>
+      </body>
     </html>
   );
 }
