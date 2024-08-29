f40a9c5a342a789b66de83d775b701f45cce0b92
Bi7al <bilal.muhammad4573@gmail.com>
2024-08-29 10:46:11 +0500

    Tailwind fix

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
             <Link href='/'><h1 className='font-extrabold text-3xl md:text-5xl text-[#3fa2f6]'>medig+</h1></Link >
 
 
-            <div className='absolute h-fit py-11 border-t-2  w-full top-16 left-0 bg-white md:static md:p-0 md:border-0 md:h-fit md:w-auto '>
+            <div className='absolute h-fit py-11 border-t-2  w-full top-16 left-0 bg-white '>
                 <ul className='flex flex-col justify-center items-center ps-4 gap-3 md:flex-row md:gap-8 text-[#2066a2]'>
                     <Link href='/panel'>
                         <li className='uppercase font-semibold hover:underline hover:transition  ease-in delay-100 hover:underline-offset-8   decoration-[#FFC94A] hover:text-[#3FA2F6]'>doctors</li>
