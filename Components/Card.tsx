5af80ecb481ca4214089bc1041791d6116030cef
Bi7al <bilal.muhammad4573@gmail.com>
2024-08-31 22:19:48 +0500

    Added Details

--- Components/Card.tsx
+++ Components/Card.tsx
@@ -8,7 +8,7 @@ interface Props {
 }
 function Card({ image, alt, title, description }: Props) {
     return (
-        <div className='w-[80%] mx-auto md:w-[25vw] h-[60vh]  flex flex-col items-center p-4 rounded-2xl shadow-[2.5px_2.5px_10px_0px] shadow-slate-500 gap-2  m-4 md:hover:scale-105 transition delay-150'>
+        <div className='w-[80%] mx-auto md:w-[25vw] h-[60vh]  flex flex-col items-center p-4 rounded-2xl shadow-[2.5px_2.5px_10px_0px] shadow-slate-500 gap-2  m-4 md:hover:scale-105 transition delay-150 duration-150 '>
             <Image
                 src={image}
                 alt={alt}
--- Components/CardContainer.tsx
+++ Components/CardContainer.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import Cardio from '@/public/heart.jpg'
+import Cardio from '@/public/heart.jpg';
+import Nuero from '@/public/Nuero.jpg';
+import Derma from '@/public/Derma.jpg';
+
 import { StaticImageData } from 'next/image';
 import Card from './Card';
 interface Card {
@@ -13,21 +16,23 @@ function CardContainer() {
         {
             image: Cardio,
             alt: 'Cardiology Department Image',
-            title: 'Cardiology Department',
-            description: 'This is a cardiology department.',
+            title: 'Cardiology ',
+            description: 'We offer a wide range of diagnostic and therapeutic services to address your heart health needs. Our state-of-the-art facilities and advanced technology ensure that you receive the highest quality care.',
 
         }, {
-            image: Cardio,
-            alt: 'Cardiology Department Image',
-            title: 'Cardiology Department',
-            description: 'This is a Nueorology department.',
+            image: Nuero,
+            alt: 'Nuerology Department Image',
+            title: 'Nuerology ',
+            description: "Our Neurology Department offers state-of-the-art diagnosis and treatment for a wide range of neurological conditions, including stroke, multiple sclerosis, Parkinson's disease, and more.",
 
         },
         {
-            image: Cardio,
-            alt: 'Cardiology Department Image',
-            title: 'Cardiology Department',
-            description: 'This is a Urology department.',
+            image: Derma,
+            alt: 'Dermatology Department Image',
+            title: 'Dermatology ',
+            description: "Our Dermatology Department is your partner in skin health. We offer expert care for a variety of skin conditions, from acne to skin cancer. Let us help you achieve your skin goals."
+
+
 
         },
     ]
